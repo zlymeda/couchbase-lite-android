c96fedd5c282d6eb97f5a9a5018e9577e9094500
hideki <hideki@couchbase.com>
2014-11-29 15:01:41 -0800

    Update test case to use Manager instead of directly create Database instance.

--- src/androidTest/java/com/couchbase/lite/DatabaseCBForestTest.java
+++ src/androidTest/java/com/couchbase/lite/DatabaseCBForestTest.java
@@ -2,9 +2,7 @@ package com.couchbase.lite;
 
 
 import com.couchbase.lite.util.Log;
-import com.couchbase.test.lite.LiteTestCaseBase;
 
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -14,8 +12,10 @@ import java.util.Map;
 /**
  * Created by hideki on 11/21/14.
  */
-public class DatabaseCBForestTest  extends LiteTestCaseBase/*LiteTestCase*/{
+public class DatabaseCBForestTest  extends /*LiteTestCaseBase*/LiteTestCase{
     public static final String TAG = "DatabaseCBForestTest";
+
+    /*
     protected String DEFAULT_TEST_DB = "cblite-test";
     protected String dbFilePath = null;
     protected Context context = new LiteTestContext();
@@ -44,9 +44,10 @@ public class DatabaseCBForestTest  extends LiteTestCaseBase/*LiteTestCase*/{
         Log.i(TAG, "tearDown");
         super.tearDown();
     }
+*/
 
     public void testHelloWorld() throws Exception{
-
+/*
         // Create database without Manager
         Database database = new DatabaseCBForest(dbFilePath, null);
         assertNotNull(database);
@@ -55,7 +56,7 @@ public class DatabaseCBForestTest  extends LiteTestCaseBase/*LiteTestCase*/{
         assertEquals(database.getPath(), dbFilePath);
         assertTrue(database.open());
         Log.i(TAG, database.getPath());
-
+*/
         Log.w(TAG, "Create a document");
 
         // get the current date and time
@@ -125,56 +126,11 @@ public class DatabaseCBForestTest  extends LiteTestCaseBase/*LiteTestCase*/{
         } catch (CouchbaseLiteException e) {
             Log.e (TAG, "Cannot delete document", e);
         }
-
+/*
         // delete database
         database.delete();
         // close database
         assertTrue(database.close());
+*/
     }
-
-/*
-    // From CBL Android Tutorial
-    public void testSimple() throws Exception {
-        Log.i(TAG, "DatabaseCBForestTest.testSimple() - START");
-
-        // get the current date and time
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar calendar = GregorianCalendar.getInstance();
-        String currentTimeString = dateFormatter.format(calendar.getTime());
-
-        // create an object that contains data for a document
-        Map<String, Object> docContent = new HashMap<String, Object>();
-        docContent.put("message", "Hello Couchbase Lite");
-        docContent.put("creationDate", currentTimeString);
-
-        // display the data for the new document
-        Log.i(TAG, "docContent=" + String.valueOf(docContent));
-
-        // create an empty document
-        Document document = database.createDocument();
-        assertNotNull(document);
-
-        // write the document to the database
-        try {
-            document.putProperties(docContent);
-        } catch (CouchbaseLiteException e) {
-            Log.e(TAG, "Cannot write document to database", e);
-        }
-
-        // save the ID of the new document
-        String docID = document.getId();
-        assertNotNull(docID);
-
-        Log.i(TAG, "docID=" + docID);
-
-        // retrieve the document from the database
-        Document retrievedDocument = database.getDocument(docID);
-        assertNotNull(retrievedDocument);
-
-        // display the retrieved document
-        Log.i(TAG, "retrievedDocument=" + String.valueOf(retrievedDocument.getProperties()));
-
-        Log.i(TAG, "DatabaseCBForestTest.testSimple() - END");
-    }
-    */
 }
